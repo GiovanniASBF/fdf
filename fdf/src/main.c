bc174c290c25fc3d53c574633279374998b8b896
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-11-10 11:24:19 -0300

    Feat: ADD argument count validation

--- fdf/src/main.c
+++ fdf/src/main.c
@@ -6,8 +6,20 @@
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 11:03:35 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/11/10 11:05:19 by gaguiar-         ###   ########.fr       */
+/*   Updated: 2025/11/10 11:21:22 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		ft_printf("Error: Wrong number of arguments.\n");
+		ft_printf("Usage: ./fdf <map_file.fdf>\n");
+		return (1);
+	}
+	
+	return (0);
+}
